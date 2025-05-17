4d2b7583784a173cef0d0b14e47853656a756b91
kiiiiiiiven <115597443+kiiiiiiiven@users.noreply.github.com>
2025-05-17 12:17:09 +0800

    Add files via upload

---
+++ gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','berkeley.edu','uct.ac.za','indiana.edu','columbia.edu','ufp.pt','virginia.edu','etudes.org','gmail.com'],
+['2005-12',25,6,5,7,4,4,7,5,1,4],
+['2006-01',97,33,29,25,26,27,24,21,24,21],
+['2006-02',2,0,1,1,2,0,0,1,1,0]
+];
